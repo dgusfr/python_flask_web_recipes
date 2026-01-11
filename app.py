32ca68fa7cea66f0f492bd31eb459fa8e9461251
dgusfr <diegogustavo.fr@gmail.com>
2026-01-11 18:39:16 -0300

    Refactor receita route to improve data loading and structure

--- app.py
+++ app.py
@@ -14,22 +14,31 @@ def home():
 
 @app.route("/receita/<int:id>")
 def receita(id):
-    # 1. Carrega os dados (igual na home)
     with open("dados.json", encoding="utf-8") as arquivo:
         lista_receitas = json.load(arquivo)
 
-    # 2. Procura a receita correta pelo ID
     receita_encontrada = None
     for item in lista_receitas:
         if item["id"] == id:
             receita_encontrada = item
             break
 
-    # 3. Se achou, mostra o template. Se não, erro 404 (básico)
     if receita_encontrada:
         return render_template("receita.html", receita=receita_encontrada)
     else:
         return "Receita não encontrada!", 404
+
+
+@app.route("/sobre")
+def about():
+    with open("dados.json", encoding="utf-8") as arquivo:
+        lista_receitas = json.load(arquivo)
+    return render_template("about.html", receitas=lista_receitas[:3])
+
+
+@app.route("/contato")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
